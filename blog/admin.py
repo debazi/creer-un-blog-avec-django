9a4e5fc1959074bd4ff3121b8eb7ac2001d3633d
debazi <marcdebazi@gmail.com>
2021-01-28 16:49:00 +0000

    création d'un fichier du repertoire templates et des vues

--- blog/admin.py
+++ blog/admin.py
@@ -9,5 +9,12 @@ from .models import Post
 
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
 
